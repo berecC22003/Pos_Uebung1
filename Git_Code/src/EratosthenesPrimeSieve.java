867d6dfec4bd51bf8f00f83437d21e681330ada8
berecC22003 <berecC22003@sus.htl-grieskirchen.at>
2024-09-17 14:46:08 +0200

    All methods completed and finished; all Methods OK

--- Git_Code/src/EratosthenesPrimeSieve.java
+++ Git_Code/src/EratosthenesPrimeSieve.java
@@ -1,3 +1,42 @@
-public class EratosthenesPrimeSieve {
-    
+import java.util.Arrays;
+
+public class EratosthenesPrimeSieve implements PrimeSteve {
+
+    private int limit;
+    private boolean[] sieve;
+
+    public EratosthenesPrimeSieve(int limit) {
+        this.limit = limit;
+        this.sieve = new boolean[limit + 1];
+        Arrays.fill(sieve, true);
+        sieve[0] = false;
+        sieve[1] = false;
+
+        for (int i = 2; i * i <= limit; i++) {
+            if (sieve[i]) {
+                for (int j = i * i; j <= limit; j += i) {
+                    sieve[j] = false;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        if (p < 0 || p > limit) {
+            throw new IllegalArgumentException("Number is out of bounds");
+        }
+        return sieve[p];
+    }
+
+    @Override
+    public void printPrimes() {
+        System.out.println("Prime numbers up to " + limit + ":");
+        for (int i = 2; i <= limit; i++) {
+            if (sieve[i]) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
 }
--- Git_Code/src/Main.java
+++ Git_Code/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int limit = 50;
+        EratosthenesPrimeSieve sieve = new EratosthenesPrimeSieve(limit);
 
+        sieve.printPrimes();
+
+        System.out.println("Is 47 prime? " + sieve.isPrime(47));
+        System.out.println("Is 48 prime? " + sieve.isPrime(48));
     }
 }
